3ecfc5159d3c27083f712efddbd928d1d40bcdc1
asif-sanix <asif.sanix@gmail.com>
2018-05-22 17:40:50 +0530

    css and edit delete

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="container">
 <div class="card   ">
-  <div class="card-header bg-primary text-white">Companies <a class="float-right btn btn-primary" href="/companies/create">Add Company</a></div>
+  <div class="card-header bg-primary text-white"><span class="float-left btn btn-primary">Contacts </span> <a class="float-right btn btn-primary" href="/contacts/create">Add Contact</a></div>
   <div class="card-body">
     <ul class="list-group">
        @foreach ($contacts as $contact)
-         <li class="list-group-item"><a href="/companies/{{$contact->id}}"> {{ $contact->full_name }}</a> {{ $contact->mobile_no }} </li>
+         <li class="list-group-item"><div class="d-table"> <span class="d-table-cell text-left"><a href="/companies/{{$contact->id}}"> {{ $contact->full_name }}</a></span> <span class="d-table-cell text-center"> {{ $contact->mobile_no }} </span> <span class="d-table-cell text-center"> {{ $contact->contact_email }} </span> <span class="d-table-cell text-right"> Edit / Delete </span> </div> </li>
        @endforeach
    </ul>
 
